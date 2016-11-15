e6afcd8f9062140382ac6ca8b90a65ff8e07f28b
gdmec07150839 <178991846@qq.com>
2016-11-15 22:25:39 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/s07150839gdmec/mediapplayerserver/MainActivity.java
@@ -0,0 +1,36 @@
+package cn.edu.s07150839gdmec.mediapplayerserver;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,1,0,"使用服务器播放音乐");
+        menu.add(0,2,0,"停止播放");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent=new Intent(this,PlayService.class);
+        switch (item.getItemId()){
+            case 1:
+                startService(intent);
+                break;
+            case 2:
+                stopService(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
